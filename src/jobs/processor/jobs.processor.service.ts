872851b468acbec9ad166e182c72aa841558a7b5
Luis Alho <lmalho@gmail.com>
2023-09-24 10:14:18 +0100

    feat: add processor

---
+++ src/jobs/processor/jobs.processor.service.ts
@@ -0,0 +1,21 @@
+import { Process, Processor } from '@nestjs/bull';
+import { JobPayload, Queues } from 'src/common/types/queues';
+import { JobsService } from '../jobs.service';
+import { Job } from 'bull';
+import { JobStatus } from 'src/jobs/types/job.types';
+
+@Processor(Queues.JOB)
+export class JobsProcessor {
+  constructor(private readonly jobsService: JobsService) {}
+
+  @Process()
+  async process(job: Job<JobPayload>): Promise<any> {
+    console.log(`Processing new job ${job.id}`);
+
+    const jobUpdated = await this.jobsService.update(job.id.toString(), {
+      status: JobStatus.FINISHED,
+    });
+    if (jobUpdated) console.log(`Updated export job ${job.id}`);
+    return;
+  }
+}
