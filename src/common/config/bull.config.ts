7a4a18c2cd23594b966f8212d0a632ac6a332139
Luis Alho <lmalho@gmail.com>
2023-09-26 19:36:56 +0100

    feat: update bull config

--- src/common/config/bull.config.ts
+++ src/common/config/bull.config.ts
@@ -1,14 +1,18 @@
 import { registerAs } from '@nestjs/config';
 import { QueueOptions } from 'bull';
 
-export default registerAs<QueueOptions>('bull', (): QueueOptions => {
-  const { REDIS_URL, REDIS_PORT } = process.env;
+export const BullConfigKey = 'bull';
+export const BullConfig = registerAs<QueueOptions>(
+  BullConfigKey,
+  (): QueueOptions => {
+    const { REDIS_URL, REDIS_PORT } = process.env;
 
-  const options: QueueOptions = {
-    redis: {
-      host: REDIS_URL,
-      port: +REDIS_PORT,
-    },
-  };
-  return options;
-});
+    const options: QueueOptions = {
+      redis: {
+        host: REDIS_URL,
+        port: +REDIS_PORT,
+      },
+    };
+    return options;
+  },
+);
